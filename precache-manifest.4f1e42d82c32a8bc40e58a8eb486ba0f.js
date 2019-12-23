42982dfdef09d16c70fc424114e3d518f2d698a8
root <root@website.europe-west2-c.c.third-fold-262923.internal>
2019-12-23 13:36:12 +0000

    Updates

---
+++ precache-manifest.4f1e42d82c32a8bc40e58a8eb486ba0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b5a92878c3559358ba7111ac68fa11",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "424f5da780c710052352",
+    "url": "/hangman/static/css/main.219e8a0f.chunk.css"
+  },
+  {
+    "revision": "5a24a4d87377b5283f3c",
+    "url": "/hangman/static/js/2.4a80f5eb.chunk.js"
+  },
+  {
+    "revision": "424f5da780c710052352",
+    "url": "/hangman/static/js/main.442f61da.chunk.js"
+  },
+  {
+    "revision": "19ffbe7598942725a8e1",
+    "url": "/hangman/static/js/runtime~main.d9fce119.js"
+  }
+]);
